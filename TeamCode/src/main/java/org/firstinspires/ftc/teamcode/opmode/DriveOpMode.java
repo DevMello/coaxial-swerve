b993d321d1f447d19faa8d3cbc3b236d3fbd909a
devmello <kol.kid.py@gmail.com>
2023-03-26 10:48:32 -0700

    modified DriveOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveOpMode.java
@@ -1,16 +1,16 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.commands.drive.DriveRobotCentric;
+import org.firstinspires.ftc.teamcode.commands.drive.DriveRobotCentricSwerve;
 
 @TeleOp(name = "Swerve Drive Test", group = "TeleOp")
 public class DriveOpMode extends DriveBaseOpMode{
-    private DriveRobotCentric robotCentricDrive;
+    private DriveRobotCentricSwerve robotCentricDrive;
 
     @Override
     public void initialize() {
         super.initialize();
-        robotCentricDrive = new DriveRobotCentric(drive, gamepadEx1::getLeftX ,
+        robotCentricDrive = new DriveRobotCentricSwerve(drive, gamepadEx1::getLeftX ,
                 gamepadEx1::getLeftY, gamepadEx1::getRightX);
         register(drive);
         drive.setDefaultCommand(robotCentricDrive);
